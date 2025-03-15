119a00bbc77998b8307439fa1b3dde0e8b8fbdf2
Enock Tangus <tanguskipngeno@gmail.com>
2025-03-15 18:53:22 +0300

    using array map method lab complete

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = function titleCased() {
+  return tutorials.map(tutorial => 
+    tutorial.split(" ").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+  );
 }
+
+console.log(titleCased());
